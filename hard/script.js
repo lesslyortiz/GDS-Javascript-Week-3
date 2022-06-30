9c56fad07101eb67947d38b3dd49a712ab06565a
Lessly Ortiz <79596365+lesslyortiz@users.noreply.github.com>
2022-06-30 14:59:19 -0400

    Create script.js

---
+++ hard/script.js
@@ -0,0 +1,16 @@
+// HARD: Create 4 math functions, one called add() that adds 2 numbers, one called subtract() that subtracts 2 numbers, one called multiply() to multiply 2 numbers, and one called divide() to divide two numbers.
+function add(n1, n2) {
+  return (n1 + n2);   
+}
+
+function subtract(n1, n2) {
+  return (n1 - n2);   
+}
+
+function multiply(n1, n2) {
+  return (n1 * n2);   
+}
+
+function divide(n1, n2) {
+  return (n1 / n2);   
+}
