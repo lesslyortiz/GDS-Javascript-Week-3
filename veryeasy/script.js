39d26961438cebc8d8a6b5544b422fb3b5b11a0a
Lessly Ortiz <79596365+lesslyortiz@users.noreply.github.com>
2022-06-30 14:55:32 -0400

    Create script.js

---
+++ veryeasy/script.js
@@ -0,0 +1,5 @@
+// VERY EASY: Create two variables and assign a number to each. Console log the difference between the numbers. Example output: “The difference between 6 and 2 is 4”
+var num1 = 2;
+var num2 = 3;
+console.log(num1 - num2);
+
